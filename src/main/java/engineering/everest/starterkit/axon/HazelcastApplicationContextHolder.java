588febe1db201d94538f2cb3a87fa8b473f177e3
Sebastian Luehr <sebastian.luehr@everest.engineering>
2020-03-12 14:56:10 +1100

    Reinstated @Component annotation on HazelcastApplicationContextHolder, the absence of which broke command dispatching.

--- src/main/java/engineering/everest/starterkit/axon/HazelcastApplicationContextHolder.java
+++ src/main/java/engineering/everest/starterkit/axon/HazelcastApplicationContextHolder.java
@@ -3,7 +3,9 @@ package engineering.everest.starterkit.axon;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class HazelcastApplicationContextHolder implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
